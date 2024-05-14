61185cb4d3ba1b62da665e736aa66adb66a9e6b4
naumovdk <dimnau@gmail.com>
2024-05-14 11:33:31 +0300

    patch

--- download-sdk.mjs
+++ download-sdk.mjs
@@ -19,10 +19,11 @@ import { execSync } from "node:child_process"
 const dir = "./interface_sdk-js"
 
 if (fs.existsSync(dir)) {
-    //execSync(`cd ${dir} && git pull`)
+    // execSync(`cd ${dir} && git pull`)
 } else {
     console.log("Downloading sdk")
     execSync("git clone --depth=1 https://gitee.com/openharmony/interface_sdk-js.git")
+    execSync(`cd ${dir} && git apply ../patches-sdk/*.patch`)
 }
 
 let sdk = "./sdk"
