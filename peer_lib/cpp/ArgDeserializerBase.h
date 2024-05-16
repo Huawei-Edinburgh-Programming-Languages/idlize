f5f21e3e79b08a84d08561e0b430586196c498d9
vadimdolgachev <vadimdolgachev@gmail.com>
2024-05-16 18:34:28 +0700

    added writing runtime type of Length

--- peer_lib/cpp/ArgDeserializerBase.h
+++ peer_lib/cpp/ArgDeserializerBase.h
@@ -276,6 +276,7 @@ public:
     Ark_Length result;
     Ark_Tag tag = readTag();
     if (tag == Ark_Tag::ARK_TAG_LENGTH) {
+      result.type = readInt8();
       result.value = readFloat32();
       result.unit = readInt32();
       result.resource = readInt32();
