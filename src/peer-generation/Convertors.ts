e596e92878ac2447d952e00209ede1958f94db4d
vadimdolgachev <vadimdolgachev@gmail.com>
2024-06-02 19:53:23 +0700

    creating Length and number object type correctly

--- src/peer-generation/Convertors.ts
+++ src/peer-generation/Convertors.ts
@@ -305,9 +305,11 @@ export class LengthConvertor extends BaseArgConvertor {
         )
     }
     convertorDeserialize(param: string, value: string, printer: LanguageWriter): LanguageStatement {
-        const accessor = printer.getObjectAccessor(this, param, value)
-        return printer.makeAssign(accessor, undefined,
-            printer.makeString(`${param}Deserializer.readLength()`), false)
+        const receiver = printer.getObjectAccessor(this, param, value)
+        return printer.makeAssign(receiver, undefined,
+            printer.makeCast(
+                printer.makeString(`${param}Deserializer.readLength()`),
+                printer.makeType(this.tsTypeName, false, receiver), false), false)
     }
     nativeType(impl: boolean): string {
         return PrimitiveType.Length.getText()
@@ -376,10 +378,10 @@ export class UnionConvertor extends BaseArgConvertor {
         const selectorAssign = printer.makeAssign(selector, Type.Int32,
             printer.makeString(`${param}Deserializer.readInt8()`), true)
         const branches: BranchStatement[] = this.memberConvertors.map((it, index) => {
-            const accessor = printer.getObjectAccessor(this, param, value, {index: `${index}`})
+            const receiver = printer.getObjectAccessor(this, param, value, {index: `${index}`})
             const expr = printer.makeString(`${selector} == ${index}`)
             const stmt = new BlockStatement([
-                it.convertorDeserialize(param, accessor, printer),
+                it.convertorDeserialize(param, receiver, printer),
                 printer.makeSetUnionSelector(value, `${index}`)
             ], false)
             return { expr, stmt }
@@ -882,8 +884,11 @@ export class NumberConvertor extends BaseArgConvertor {
         printer.writeMethodCall(`${param}Serializer`, "writeNumber", [value])
     }
     convertorDeserialize(param: string, value: string, writer: LanguageWriter): LanguageStatement {
-        const accessor = writer.getObjectAccessor(this, param, value)
-        return writer.makeAssign(accessor, undefined, writer.makeString(`${param}Deserializer.readNumber()`), false)
+        const receiver = writer.getObjectAccessor(this, param, value)
+        return writer.makeAssign(receiver, undefined,
+            writer.makeCast(
+                writer.makeString(`${param}Deserializer.readNumber()`),
+                writer.makeType(this.tsTypeName, false, receiver)), false)
     }
     nativeType(): string {
         return PrimitiveType.Number.getText()
