42f9828ccc950c177de1f214d25aa7918a0bd708
vadimdolgachev <vadimdolgachev@gmail.com>
2024-06-04 13:05:53 +0700

    added import of DateConvertor

--- src/peer-generation/DeclarationTable.ts
+++ src/peer-generation/DeclarationTable.ts
@@ -19,9 +19,9 @@ import { Language, asString, getDeclarationsByNode, getNameWithoutQualifiersRigh
 import { IndentedPrinter } from "../IndentedPrinter"
 import { PeerGeneratorConfig } from "./PeerGeneratorConfig"
 import {
-    AggregateConvertor, ArgConvertor, ArrayConvertor, BooleanConvertor, ClassConvertor, CustomTypeConvertor,
+    AggregateConvertor, ArgConvertor, ArrayConvertor, BooleanConvertor, ClassConvertor, CustomTypeConvertor, DateConvertor,
     EnumConvertor, FunctionConvertor, ImportTypeConvertor, InterfaceConvertor, LengthConvertor, MapConvertor, MaterializedClassConvertor,
-    NumberConvertor, OptionConvertor, PredefinedConvertor, StringConvertor, ToStringConvertor, TupleConvertor, TypeAliasConvertor,
+    NumberConvertor, OptionConvertor, PredefinedConvertor, StringConvertor, TupleConvertor, TypeAliasConvertor,
     UndefinedConvertor, UnionConvertor
 } from "./Convertors"
 import { DependencySorter } from "./DependencySorter"
