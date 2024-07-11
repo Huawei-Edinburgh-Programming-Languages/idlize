0b0a65f659680112b13ecf4b161e267a601d89dc
Korobeinikov Evgeny <korobeinikov.evgeny@huawei.com>
2024-07-11 13:42:34 +0300

    publish idl npm package

---
+++ tools/publish.mjs
@@ -0,0 +1,34 @@
+import fs from "fs"
+import chalk from "chalk"
+import path from "path"
+import process from "process"
+import { execSync } from "child_process"
+
+const CWD = process.cwd()
+const prebuiltPath = path.join(CWD, ".packages")
+
+function pack() {
+    if (fs.existsSync(prebuiltPath))
+        fs.rmSync(prebuiltPath, { recursive: true })
+    fs.mkdirSync(prebuiltPath)
+    execCmdSync(`npm pack --pack-destination ${ prebuiltPath }`)
+}
+
+function publish() {
+    pack()
+
+    let packageName = fs.readdirSync(prebuiltPath)[0]
+
+    console.log(chalk.green(`> Publishing ${ packageName }...`))
+    execCmdSync(`npm publish ${ path.join(prebuiltPath, packageName) } --registry https://nexus.bz-openlab.ru:10443/repository/koala-npm/${packageName}`)
+}
+
+function execCmdSync(cmd, options) {
+    try {
+        execSync(cmd, options)
+    } catch (e) {
+        // empty
+    }
+}
+
+publish()
