febd01d6618ce2c47898e48524e8532725cad4f8
Nikolay Igotti <Nikolay.Igotti@huawei.com>
2024-08-06 20:01:01 +0300

    Tracker

--- src/peer-generation/Tracker.ts
+++ src/peer-generation/Tracker.ts
@@ -12,11 +12,60 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import * as fs from 'fs'
+import * as path from 'path'
 
 import { PeerLibrary } from "./PeerLibrary";
+import { IndentedPrinter } from "../IndentedPrinter";
+import { PeerClass } from "./PeerClass";
+import { MaterializedClass } from "./Materialized";
+import { EnumEntity } from './PeerFile';
 
-import * as fs from 'fs'
+export class TrackerVisitor {
+    out = new IndentedPrinter()
 
-export function generateTracker(outDir: string, library: PeerLibrary): void {
+    constructor(
+        protected library: PeerLibrary
+    ) { }
+
+    printPeerClass(clazz: PeerClass): void {
+        this.out.print(`|*${clazz.componentName}*| *Component* | |`)
+        clazz.methods.forEach(method => {
+            this.out.print(`|\`${method.method.name}\`| Function | |`)
+        })
+    }
+
+    printMaterializedClass(clazz: MaterializedClass) {
+        this.out.print(`|*${clazz.className}*| *Class* | |`)
+        clazz.methods.forEach(method => {
+            this.out.print(`|\`${method.method.name}\`| Function | |`)
+        })
+    }
+
+    printEnum(enam: EnumEntity) {
+        this.out.print(`## Enum ${enam.name}`)
+    }
+
+    print() {
+        this.out.print(`# All components`)
+
+        this.out.print(`Name | Kind | Owner`)
+        this.out.print(`---- | ---- | -----`)
+
+        this.library.files.forEach(file => {
+            file.peers.forEach(clazz => this.printPeerClass(clazz))
+            //file.enums.forEach(enam => this.printEnum(enam))
+
+        })
+        this.library.materializedClasses.forEach(clazz => {
+            this.printMaterializedClass(clazz)
+        })
+    }
+}
+
+export function generateTracker(outDir: string, peerLibrary: PeerLibrary): void {
     if (!fs.existsSync(outDir)) fs.mkdirSync(outDir)
+    const visitor = new TrackerVisitor(peerLibrary)
+    visitor.print()
+    visitor.out.printTo(path.join(outDir, "COMPONENTS.md"))
 }
