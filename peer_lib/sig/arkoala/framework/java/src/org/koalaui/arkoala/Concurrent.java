76e729268c2a2cdb822f168ef3610eab35d323f4
Nikolay Igotti <igotti@gmail.com>
2024-08-06 08:19:35 +0300

    Map/reduce + cost function

--- peer_lib/sig/arkoala/framework/java/src/org/koalaui/arkoala/Concurrent.java
+++ peer_lib/sig/arkoala/framework/java/src/org/koalaui/arkoala/Concurrent.java
@@ -18,7 +18,10 @@ package org.koalaui.arkoala;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.BlockingQueue;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.function.Supplier;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 class Node {
     int kind;
@@ -30,6 +33,10 @@ class Node {
         this.kind = kind;
     }
     static Node create(int kind) {
+        return new Node(kind, 0);
+    }
+    static Node createWithCost(int kind, long cost) {
+        payCost(cost);
         return new Node(kind, 0);
     }
     void insertChildAfter(Node child, Node sibling) {
@@ -44,8 +51,39 @@ class Node {
             }
         }
     }
+    void insertChildAfterWithCost(Node child, Node sibling, long cost) {
+        payCost(cost);
+        insertChildAfter(child, sibling);
+    }
+
     void removeChild(Node child) {
         this.children.remove(child);
+    }
+    void removeChildWithCost(Node child, long cost) {
+        payCost(cost);
+        removeChild(child);
+    }
+
+    void dump() {
+        dumpWithIndent(0);
+    }
+    private void dumpWithIndent(int indent) {
+        for (int i = 0; i < indent; i++) {
+            System.out.print("  ");
+        }
+        System.out.println("->" + this.kind + " [" + this.id + "]");
+        for (Node child : children) {
+            child.dumpWithIndent(indent + 1);
+        }
+    }
+
+    static volatile long sink = 0;
+    static void payCost(long count) {
+        long store = 17;
+        for (long i = 0; i < count * 1000; i++) {
+            store += i * 79 % 31;
+        }
+        sink += store;
     }
 }
 
@@ -67,11 +105,12 @@ class WorkerResult {
 
 class CreateTreeTask implements WorkerTask {
     Supplier<Node> builder;
-    CreateTreeTask(Supplier<Node> builder) {
+    CreateTreeTask(Supplier<Node> builder, int breadth, int depth) {
         this.builder = builder;
     }
     public WorkerResult run(Worker worker) {
-        return new WorkerResult(this.builder.get());
+        Node root = builder.get();
+        return new WorkerResult(root);
     }
 }
 
@@ -90,7 +129,9 @@ class Worker implements Runnable {
     final BlockingQueue<WorkerTask> queue = new LinkedBlockingDeque<WorkerTask>();
     volatile boolean stopped = false;
     ResultConsumer consumer;
-    Worker(ResultConsumer consumer) {
+    Random random;
+    Worker(int index, ResultConsumer consumer) {
+        this.random = new Random(index * 239);
         this.consumer = consumer;
     }
     public void run() {
@@ -112,12 +153,11 @@ public class Concurrent implements ResultConsumer {
     public static void main(String[] args) {
         new Concurrent(30, 10, 4).start();
     }
-
     int breadth;
     int depth;
     int numWorkers;
     Worker[] workers;
-    final BlockingQueue queue = new LinkedBlockingDeque<WorkerResult>();
+    final BlockingQueue<WorkerResult> queue = new LinkedBlockingDeque<WorkerResult>();
 
     Concurrent(int breadth, int depth, int numWorkers) {
         this.breadth = breadth;
@@ -125,25 +165,55 @@ public class Concurrent implements ResultConsumer {
         this.numWorkers = numWorkers;
         this.workers = new Worker[numWorkers];
         for (int i = 0; i < numWorkers; i++) {
-            Worker worker = new Worker(this);
+            Worker worker = new Worker(i, this);
             this.workers[i] = worker;
             new Thread(worker).start();
         }
     }
 
-    void start() {
+    void map(Function<Integer, WorkerTask> supplier) {
         for (int i = 0; i < numWorkers; i++) {
-            this.workers[i].add(new StopTask());
-        }
-        for (int i = 0; i < numWorkers; i++) {
-            try {
-                this.queue.take();
-            } catch (InterruptedException e) {}
+            this.workers[i].add(supplier.apply(i));
         }
     }
 
+    void reduce(Consumer<WorkerResult[]> consumer) {
+        WorkerResult[] result = new WorkerResult[numWorkers];
+        for (int i = 0; i < numWorkers; i++) {
+            try {
+                result[i] = this.queue.take();
+            } catch (InterruptedException e) {}
+        }
+        consumer.accept(result);
+    }
+
+
+    void start() {
+        Node root = Node.create(1);
+        mapReduce("create",
+            (index) -> new CreateTreeTask(() -> Node.createWithCost(2, 100), breadth, depth),
+            (result) -> {
+                for (WorkerResult r : result) {
+                    root.insertChildAfterWithCost((Node)r.get(), null, 10);
+                }
+            }
+        );
+        root.dump();
+        mapReduce("stop",
+            (index) -> new StopTask(),
+            (result) -> {}
+        );
+    }
+
+    void mapReduce(String name, Function<Integer, WorkerTask> supplier, Consumer<WorkerResult[]> consumer) {
+        long start = System.nanoTime();
+        map(supplier);
+        reduce(consumer);
+        long end = System.nanoTime();
+        System.out.println(name + ": " + (end - start) + "ns");
+    }
+
     public void provide(WorkerResult result) {
-        System.out.println("provided to main: " + result);
         this.queue.add(result);
     }
 }
