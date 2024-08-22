a90ccf931260f632a96af2baf9d87ec0b6a903ea
Sokolova Irina <sokolova.irina@huawei-partners.com>
2024-08-22 12:37:27 +0000

    update src/main.ts.
    Signed-off-by: Sokolova Irina <sokolova.irina@huawei-partners.com>

--- src/main.ts
+++ src/main.ts
@@ -511,6 +511,7 @@ function generateArkoala(outDir: string, peerLibrary: PeerLibrary, lang: Languag
             const outPeerFile = arkoala.peer(targetFile)
             console.log("producing", outPeerFile)
             writeFile(outPeerFile, peer, true)
+        }
     }
 
     const components = printComponents(peerLibrary)
