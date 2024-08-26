507a173bd432ff12f1fd57e69d27cea07905190c
Sokolova Irina <sokolova.irina@huawei-partners.com>
2024-08-26 12:06:14 +0000

    update SkoalaPrinter
    Signed-off-by: Sokolova Irina <sokolova.irina@huawei-partners.com>

--- src/peer-generation/printers/SkoalaPrinter.ts
+++ src/peer-generation/printers/SkoalaPrinter.ts
@@ -1,66 +1,51 @@
 import * as fs from "fs"
 import * as path from "path"
-
-interface IDLMethod {
-    name: string
-    returnType: string
-    parameters: { name: string; type: string }[]
-}
+import { IDLEntry, IDLMethod, IDLKind, IDLInterface } from "../../idl";
 
 export class SkoalaCCodeGenerator {
-    private idlContent: string
+    private entries: IDLEntry[]
     private outputDir: string
     private fileName: string
 
-    constructor(idlContent: string, outputDir: string, fileName: string) {
-        this.idlContent = idlContent
+    constructor(entries: IDLEntry[], outputDir: string, fileName: string) {
+        this.entries = entries
         this.outputDir = outputDir
         this.fileName = fileName
     }
 
     public generate(): void {
-        const parsedMethods = this.parseIDL(this.idlContent)
-        if (parsedMethods.length === 0) {
+        const methods = this.extractMethods(this.entries)
+        if (methods.length === 0) {
             return
         }
-        const cCode = this.generateCCode(parsedMethods)
+        const cCode = this.generateCCode(methods)
         this.saveCCode(cCode)
     }
 
-    private parseIDL(idlContent: string): IDLMethod[] {
-        const methods: IDLMethod[] = []
-        const methodRegex = /(\w+)\s+(\w+)\(([^)]*)\);/g
-        let match
-
-        while ((match = methodRegex.exec(idlContent)) !== null) {
-            const returnType = match[1]
-            const methodName = match[2]
-            const params = match[3]
-                .split(",")
-                .map(param => param.trim())
-                .filter(param => param)
-                .map(param => {
-                    const [type, name] = param.split(/\s+/)
-                    return { name, type }
-                })
-
-            methods.push({ name: methodName, returnType, parameters: params })
-        }
-
-        return methods
-    }
-
+    private extractMethods(entries: IDLEntry[]): IDLMethod[] {
+        return entries
+            .filter(entry => entry.kind === IDLKind.Interface || entry.kind === IDLKind.Class)
+            .flatMap(entry => (entry as IDLInterface).methods || [])
+            .filter((method: IDLMethod) => method.kind === IDLKind.Method);
+    }    
+    
     private generateCCode(methods: IDLMethod[]): string {
-        let cCode = `#include <stdio.h>\n#include <stdlib.h>\n\n`
-
+        let cCode = `#include <stdio.h>\n#include <stdlib.h>\n\n`;
+    
         methods.forEach(method => {
-            cCode += `void ${method.name}(`
-            cCode += method.parameters.map(param => `${this.convertType(param.type)} ${param.name}`).join(", ")
-            cCode += `) {\n    // TODO: Implement ${method.name}\n}\n\n`
-        })
-
-        return cCode
+            cCode += `void ${method.name}(`;
+            cCode += method.parameters
+                .map(param => {
+                    const typeName = param.type ? this.convertType(param.type.name) : "void*";
+                    return `${typeName} ${param.name}`;
+                })
+                .join(", ");
+            cCode += `) {\n    // TODO: Implement ${method.name}\n}\n\n`;
+        });
+    
+        return cCode;
     }
+    
 
     private convertType(idlType: string): string {
         const typeMapping: { [key: string]: string } = {
@@ -72,21 +57,22 @@ export class SkoalaCCodeGenerator {
             "void_": "void",
         }
 
-        return typeMapping[idlType] || "void*"
+        const convertedType = typeMapping[idlType] || "void*"
+        return convertedType
     }
 
     private saveCCode(cCode: string): void {
         const baseFileName = path.basename(this.fileName, ".d.ts")
         const outputFileName = `${baseFileName}.cc`
         const outputPath = path.join(this.outputDir, outputFileName)
-    
+
         console.log("Saving C Code to:", outputPath)
-    
+
         try {
             fs.writeFileSync(outputPath, cCode)
             console.log("C code generated and saved to:", outputPath)
         } catch (error) {
             console.error("Error saving C code:", error)
         }
-    }    
+    }
 }
