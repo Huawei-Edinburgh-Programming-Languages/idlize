290c82e5f17a9ac54ffedc65ff68a2da59c47db2
pavelpozdeev <pozdeev.pavel@huawei-partners.com>
2024-09-04 13:55:32 +0300

    fix

--- src/linter.ts
+++ src/linter.ts
@@ -26,6 +26,7 @@ import {
     zip
 } from "./util"
 import { LinterWhitelist } from "./LinterWhitelist"
+import { cppKeywords } from "./languageSpecificKeywords"
 
 export enum LinterError {
     NONE,
@@ -57,27 +58,6 @@ export enum LinterError {
     EVENT_HANDLER_WITH_FUNCTIONAL_PARAM_TYPE,
     CALLBACK_WITH_FUNCTIONAL_PARAM_TYPE,
 }
-
-export const CJKeywords = [
-    'Int8', 'Int16', 'Int32', 'Int64', 'IntNative',
-    'UInt8', 'UInt16', 'UInt32', 'UInt64', 'UIntNative',
-    'Float16', 'Float32', 'Float64', 'Rune',
-    'Bool', 'Unit', 'Nothing', 'struct',
-    'enum', 'This', 'package', 'import',
-    'class', 'interface', 'func', 'main',
-    'let', 'var', 'const', 'type', 'init',
-    'this', 'super', 'if', 'else', 'case',
-    'try', 'catch', 'finally', 'for', 'do',
-    'while', 'throw', 'return', 'continue',
-    'break', 'is', 'as', 'in', 'match',
-    'from', 'where', 'extend', 'spawn',
-    'synchronized', 'macro', 'quote', 'true',
-    'false', 'static', 'public', 'private',
-    'protected', 'override', 'redef', 'abstract',
-    'open', 'operator', 'foreign', 'inout',
-    'prop', 'mut', 'unsafe', 'get', 'set',
-    'min', 'max'
-]
 
 export interface LinterMessage {
     file: ts.SourceFile
@@ -285,7 +265,7 @@ export class LinterVisitor implements GenericVisitor<LinterMessage[]> {
             this.report(name, LinterError.COMPUTED_PROPERTY_NAME, `Computed property name ${name.getText(this.sourceFile)}`)
         } else {
             let nameString = identName(name)!
-            if (LinterVisitor.cppKeywords.has(nameString)) {
+            if (cppKeywords.has(nameString)) {
                 this.report(
                     name,
                     LinterError.CPP_KEYWORDS,
@@ -343,30 +323,6 @@ export class LinterVisitor implements GenericVisitor<LinterMessage[]> {
         }
         return false
     }
-
-    // See https://en.cppreference.com/w/cpp/keyword.
-    private static cppKeywords = new Set([
-        `alignas`, `alignof`, `and`,
-        `and_eq`, `asm`, `atomic_cancel`, `atomic_commit`,
-        `atomic_noexcept`, `auto`, `bitand`, `bitor`, `bool`,
-        `break`, `case`, `catch`, `char`, `char8_t`, `char16_t`,
-        `char32_t`, `class`, `compl`, `concept`, `const`, `consteval`,
-        `constexpr`, `constinit`, `const_cast`, `continue`, `co_await`,
-        `co_return`, `co_yield`, `decltype`, `default`, `delete`, `do`,
-        `double`, `dynamic_cast`, `else`, `enum`, `explicit`, `export`,
-        `extern`, `false`, `float`, `for`, `friend`, `goto`, `if`,
-        `inline`, `int`, `long`, `mutable`, `namespace`, `new`, `noexcept`,
-        `not`, `not_eq`, `nullptr`, `operator`, `or`, `or_eq`, `private`,
-        `protected`, `public`, `reflexpr`, `register`, `reinterpret_cast`,
-        `requires`, `return`, `short`, `signed`,
-        `sizeof`, `static`, `static_assert`, `static_cast`,
-        `struct`, `switch`, `synchronized`, `template`,
-        `this`, `thread_local`, `throw`, `true`, `try`,
-        `typedef`, `typeid`, `typename`, `union`,
-        `unsigned`, `using`, `virtual`, `void`,
-        `volatile`, `wchar_t`, `while`, `xor`,
-        `xor_eq`
-    ])
 
     visitConstructor(ctor: ts.ConstructorDeclaration | ts.ConstructSignatureDeclaration): void {
         ctor.parameters.map(param => this.checkType(param.type))
