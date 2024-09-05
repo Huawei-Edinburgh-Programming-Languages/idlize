2c8e8eb0b6f1014596568c85e0cbf633fd9d5c04
Pavel Mironchik <pavel.mironchik@huawei.com>
2024-09-05 17:26:40 +0300

    fix run:java, add generics in BuilderClasses

--- src/peer-generation/BuilderClass.ts
+++ src/peer-generation/BuilderClass.ts
@@ -138,10 +138,7 @@ export function toBuilderClass(declarationTable: DeclarationTable,
     const isClass = ts.isClassDeclaration(target)
     const isInterface = ts.isInterfaceDeclaration(target)
 
-    let superClass = extractSuperElement(target)
-    if (superClass?.generics)
-        // we do not want builder class to have generics
-        superClass = new SuperElement(superClass.name)
+    const superClass = extractSuperElement(target)
 
     const fields = isClass
         ? target.members
