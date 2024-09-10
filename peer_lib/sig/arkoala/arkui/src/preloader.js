f50e37d8877ad1d3031239de4f2422a175f73982
Igor Loginov <loginov.igor1@huawer-partners.com>
2024-09-10 18:27:31 +0300

    fix

--- peer_lib/sig/arkoala/arkui/src/preloader.js
+++ peer_lib/sig/arkoala/arkui/src/preloader.js
@@ -1,6 +1,7 @@
+const path = require('path')
 const child_process = require('child_process')
 
-process.env.LD_LIBRARY_PATH = './native'
-process.env.PANDA_HOME = './external/incremental/tools/panda/node_modules/@panda/sdk'
+process.env.LD_LIBRARY_PATH = path.join(__dirname, '../native')
+process.env.PANDA_HOME = path.join(__dirname, '../external/incremental/tools/panda/node_modules/@panda/sdk')
 
-child_process.spawn('node', ['lib/loader.js', process.argv[2]], { stdio: 'inherit' })
+child_process.spawn('node', [path.join(__dirname, './loader.js'), process.argv[2]], { stdio: 'inherit' })
