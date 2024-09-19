5cd9c8e0e8ddaacf05dfd00f2dade9a3d0c44cf2
twx1232375 <twx1232375@huawei-partners.com>
2024-09-19 17:02:31 +0300

    Added copyrights

--- skoala_lib/sig/skoala/arkui/src/SerializerBase.ts
+++ skoala_lib/sig/skoala/arkui/src/SerializerBase.ts
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import { float32, int32 } from "@koalaui/common"
 import { pointer, wrapCallback} from "@koalaui/interop"
 import { nativeModule } from "@koalaui/arkoala"
