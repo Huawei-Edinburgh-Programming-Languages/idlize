389d8e8b2357e2cfa647999f9b9fd377e047d1c6
Nikolay Igotti <nikolay.igotti@huawei.com>
2024-09-20 17:34:38 +0800

    Functional

--- peer_lib/sig/arkoala/arkui/src/loader.ts
+++ peer_lib/sig/arkoala/arkui/src/loader.ts
@@ -13,10 +13,34 @@
 * limitations under the License.
 */
 
-import { nativeModule } from "@koalaui/arkoala"
 import { Worker, isMainThread, parentPort } from "node:worker_threads"
 
 type int32 = number
+
+export interface LoaderOps {
+    _LoadVirtualMachine(vmKind: int32, appClassPath: string, appLibPath: string): int32
+    _StartApplication(): int32
+    _RunApplication(arg0: int32, arg1: int32): int32
+    _SetCallbackDispatcher(dispatcher: (id: int32, args: Uint8Array, length: int32) => int32): void
+}
+
+let theModule: LoaderOps | undefined = undefined
+
+declare const LOAD_NATIVE: LoaderOps
+
+export function callCallback(id: int32, args: Uint8Array, length: int32): int32 {
+   console.log("Called callCallback()")
+   throw new Error("Not yet implemented")
+}
+
+export function nativeModule(): LoaderOps {
+    if (theModule) return theModule
+    theModule = LOAD_NATIVE as LoaderOps
+    if (!theModule)
+        throw new Error("Cannot load native module")
+    theModule._SetCallbackDispatcher(callCallback)
+    return theModule
+}
 
 function waitVSync(): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, 100) )
@@ -29,12 +53,11 @@ export async function runEventLoop() {
     }
 }
 
-function runEs2Panda(msg: { kind: string, id: int32, files: string[]}):
-    { id: int32, result: int32 } {
+function runEs2Panda(msg: WorkerRequest): WorkerResponse {
     console.log(`JS: es2panda ${msg.files}`)
     return {
         id: msg.id,
-        result: nativeModule()._LoadVirtualMachine(3, msg.files.join(","), "")
+        result: nativeModule()._LoadVirtualMachine(3, msg.files.join(","), msg.nativePath)
     }
 }
 
@@ -42,7 +65,19 @@ let workers = new Array<Worker>()
 let requests = new Map<int32, (result: int32) => void>()
 let requestId = 0
 
-function compile(worker: Worker, index: int32): Promise<int32> {
+interface WorkerRequest {
+    kind: string
+    id: int32
+    files: string[]
+    nativePath: string
+}
+
+interface WorkerResponse {
+    id: int32
+    result: int32
+}
+
+function compile(worker: Worker, nativePath: string, index: int32): Promise<int32> {
     return new Promise(resolve => {
         let id = requestId++
         requests.set(id, (result: int32) => {
@@ -52,37 +87,39 @@ function compile(worker: Worker, index: int32): Promise<int32> {
         worker.postMessage({
             kind: "compile",
             id: id,
+            nativePath: nativePath,
             files: [`file${index}.sts`, `shmile${index}.sts`]
         })
     })
 }
 
-function runEs2PandaMain(): int32 {
+function runEs2PandaMain(nativePath: string): int32 {
     if (!isMainThread) return 0
 
     for (let i = 0; i < 10; i++) {
         workers.push(new Worker(__filename))
-        workers[i].on("message", (response: {id: int32, result: int32}) => {
+        workers[i].on("message", (response: WorkerResponse) => {
             requests.get(response.id)!(response.result)
         })
     }
     setTimeout(async () => {
         await Promise.all(
-            workers.map((worker, index) => compile(worker, index)))
-    })
+            workers.map((worker, index) => compile(worker, nativePath, index)))
+        workers.forEach(worker => worker.terminate())
+    }, 0)
     return 0
 }
 
 export function initWorker() {
     // Here we're in the worker.
-    parentPort!.addListener("message", (request: any) => {
-        procesWorkerRequest(request)
+    parentPort!.addListener("message", (request: WorkerRequest) => {
+        processRequest(request)
             .then((response) => {
-                parentPort!.postMessage(response, response.transferList())
+                parentPort!.postMessage(response)
             })
     })
 
-    function procesWorkerRequest(request: { kind: string, id: int32, files: string[]}): Promise<any> {
+    function processRequest(request: WorkerRequest): Promise<WorkerResponse> {
         return new Promise((resolve, reject) => {
             if (request.kind == "compile") {
                 resolve(runEs2Panda(request))
@@ -110,7 +147,8 @@ export function checkLoader(variant: string): int32 {
             break
         }
         case 'es2panda': {
-            return runEs2PandaMain()
+            vm = 3
+            return runEs2PandaMain(nativePath)
         }
     }
     let result = nativeModule()._LoadVirtualMachine(vm, classPath, nativePath)
