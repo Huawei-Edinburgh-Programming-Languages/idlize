e5eb11b96ab7cf4a28c7383487610c27854e50a1
vadimdolgachev <dolgachev.vadim@huawei-partners.com>
2024-09-23 21:07:24 +0700

    Some fixes to compile full sdk

--- src/peer-generation/printers/PeersPrinter.ts
+++ src/peer-generation/printers/PeersPrinter.ts
@@ -226,7 +226,7 @@ class PeerFileVisitor {
             }
             case Language.ARKTS: {
                 return [...defaultPeerImports,
-                    `import { NativeModule } from "#arkui"`,]
+                    `import { NativeModule } from "#arkui/NativeModule"`,]
             }
             default: {
                 return []
