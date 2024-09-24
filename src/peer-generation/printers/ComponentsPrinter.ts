589ba1e7bc6da6a550beb8fb63b36dd1341ca9b3
vadimdolgachev <dolgachev.vadim@huawei-partners.com>
2024-09-24 14:49:22 +0700

    Added necessary types for ArkTs to TypeChecker

--- src/peer-generation/printers/ComponentsPrinter.ts
+++ src/peer-generation/printers/ComponentsPrinter.ts
@@ -94,9 +94,12 @@ class TSComponentFileVisitor implements ComponentFileVisitor {
             imports.addFeature("isResource", "./peers/SerializerBase")
             imports.addFeature("isInstanceOf", "./peers/SerializerBase")
             imports.addFeature('ComponentBase', './ComponentBase')
-            imports.addFeature('TypeChecker', '#arkui/type_check')
+
             if (this.printer.language == Language.TS)
                 imports.addFeature('unsafeCast', './shared/generated-utils')
+            else if (this.printer.language == Language.ARKTS) {
+                imports.addFeature('TypeChecker', '#arkui/type_check')
+            }
             for (const method of peer.methods) {
                 for (const argType of method.declarationTargets)
                     if (convertToCallback(peer, method, argType))
