102baa6b03ff7828a47d157c810ffc323e1f1f4b
pavelpozdeev <pozdeev.pavel@huawei-partners.com>
2024-10-29 18:56:15 +0300

    add usecases for writeProperty

--- src/peer-generation/LanguageWriters/writers/CJLanguageWriter.ts
+++ src/peer-generation/LanguageWriters/writers/CJLanguageWriter.ts
@@ -206,7 +206,7 @@ export class CJLanguageWriter extends LanguageWriter {
         this.printer.print(`}`)
     }
     writeProperty(propName: string, propType: idl.IDLType, mutable?: boolean, getterLambda?: (writer: LanguageWriter) => void, setterLambda?: (writer: LanguageWriter) => void) {
-        this.print(`${mutable ? "mut " : ""}prop ${propName}: ${convertType(this, propType).alias} {`)
+        this.print(`${mutable ? "mut " : ""}prop ${propName}: ${propType.optional ? '?' : ''}${convertType(this, propType).alias} {`)
         this.pushIndent()
         this.print(`get() {`)
         this.pushIndent()
