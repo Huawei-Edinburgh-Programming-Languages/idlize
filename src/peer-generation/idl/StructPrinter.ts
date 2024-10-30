ff81660708223185d4a85e266d63aafd20ac652b
Aleksandr Veselov <veselov.aleksandr@huawei.com>
2024-10-30 15:59:25 +0300

    Fix callbacks type names

--- src/peer-generation/idl/StructPrinter.ts
+++ src/peer-generation/idl/StructPrinter.ts
@@ -131,7 +131,7 @@ export class StructPrinter {
                         concreteDeclarations.print(`void *handle;`) // avoid empty structs
                     }
                     properties.forEach(it => {
-                        concreteDeclarations.print(`${this.library.getTypeName(it.type, it.isOptional)} ${concreteDeclarations.escapeKeyword(it.name)};`)
+                        concreteDeclarations.print(`${this.library.computeTargetName(it.type, it.isOptional)} ${concreteDeclarations.escapeKeyword(it.name)};`)
                     })
                 } else if (idl.isContainerType(target)) {
                     let fieldNames: string[] = []
