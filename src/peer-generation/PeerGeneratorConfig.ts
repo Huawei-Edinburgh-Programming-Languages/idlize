02996fd5a234c7ce941259feee49309e8c3c5e64
Sokolova Irina <sokolova.irina@huawei-partners.com>
2024-11-01 09:44:28 +0000

    update src/peer-generation/PeerGeneratorConfig.ts.
    Signed-off-by: Sokolova Irina <sokolova.irina@huawei-partners.com>

--- src/peer-generation/PeerGeneratorConfig.ts
+++ src/peer-generation/PeerGeneratorConfig.ts
@@ -35,6 +35,10 @@ export class PeerGeneratorConfig {
         "ContentSlot",
     ]
 
+    public static ignoreSerialized = [
+            "IMonitor"
+    ]
+
     private static knownParametrized = [
         "Indicator", "AttributeModifier", "AnimationRange", "ContentModifier", "SizeT", "PositionT", "Record"
     ]
@@ -166,6 +170,10 @@ export class PeerGeneratorConfig {
         if (originalName.endsWith("Attribute"))
             return originalName.substring(0, originalName.length - 9)
         return originalName
+    }
+
+    static isIgnoredSerialized(name: string | undefined) : boolean {
+        return name != undefined && PeerGeneratorConfig.ignoreSerialized.includes(name)
     }
 
     static isKnownParametrized(name: string | undefined) : boolean {
