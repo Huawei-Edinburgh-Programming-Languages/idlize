4fa92c7b820e294b59165fd1052537bc8a7638fa
Sokolova Irina <sokolova.irina@huawei-partners.com>
2024-11-02 12:56:56 +0000

    update src/idl.ts.
    Signed-off-by: Sokolova Irina <sokolova.irina@huawei-partners.com>

--- src/idl.ts
+++ src/idl.ts
@@ -1001,7 +1001,3 @@ export const DebugUtils = {
         return name
     }
 }
-
-export function isCallSignature(method: IDLCallable | IDLMethod | IDLConstructor): boolean {
-        return !isMethod(method)
-}
