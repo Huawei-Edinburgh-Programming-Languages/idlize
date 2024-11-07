43355c5ea85f2dab6b1436647907caf19eec4194
Nikolay Igotti <Nikolay.Igotti@huawei.com>
2024-11-07 18:38:11 +0300

    Ignore standard types for TS

--- src/idl.ts
+++ src/idl.ts
@@ -79,6 +79,7 @@ export enum IDLExtendedAttributes {
     TypeArguments = "TypeArguments",
     TypeParameters = "TypeParameters",
     VerbatimDts = "VerbatimDts",
+    TSType = "TSType",
 }
 
 export enum IDLAccessorAttribute {
@@ -641,9 +642,9 @@ export function createProperty(
 }
 
 export function createParameter(
-    name: string, 
-    type: IDLType | undefined, 
-    isOptional: boolean = false, 
+    name: string,
+    type: IDLType | undefined,
+    isOptional: boolean = false,
     isVariadic: boolean = false,
     nodeInitializer: IDLNodeInitializer = {},
 ): IDLParameter {
@@ -1074,7 +1075,7 @@ export const IDLContainerUtils = {
     isPromise: (x:IDLNode) => isContainerType(x) && x.containerKind === 'Promise'
 }
 
-/** 
+/**
  * @returns tuple of qualifier name and real type name
  */
 export function decomposeQualifiedName(type: IDLReferenceType): [string | undefined, string] {
