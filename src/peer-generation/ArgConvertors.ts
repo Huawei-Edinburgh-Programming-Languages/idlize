be01d08b3ae2b93e47125303e09241eed71dafa7
vadimdolgachev <dolgachev.vadim@huawei-partners.com>
2024-11-14 18:00:50 +0700

    More correct Date serialization

--- src/peer-generation/ArgConvertors.ts
+++ src/peer-generation/ArgConvertors.ts
@@ -1191,10 +1191,10 @@ export class DateConvertor extends BaseArgConvertor { //
     convertorSerialize(param: string, value: string, writer: LanguageWriter): void {
         if (writer.language === Language.CPP) {
             writer.writeMethodCall(`${param}Serializer`, "writeInt64", [value])
-        } else if (writer.language === Language.ARKTS) {
-            writer.writeMethodCall(`${param}Serializer`, "writeNumber", [`${value}.getTime() as float64`])
         } else {
-            writer.writeMethodCall(`${param}Serializer`, "writeInt64", [`${value}.getTime()`])
+            writer.writeMethodCall(`${param}Serializer`, "writeInt64", [
+                writer.makeCast(writer.makeString(`${value}.getTime()`), idl.IDLI64Type).asString()
+            ])
         }
     }
     convertorDeserialize(bufferName: string, deserializerName: string, assigneer: ExpressionAssigneer, writer: LanguageWriter): LanguageStatement {
--- src/peer-generation/FileGenerators.ts
+++ src/peer-generation/FileGenerators.ts
@@ -290,7 +290,6 @@ export function makeTSSerializer(library: PeerLibrary): LanguageWriter {
         imports.addFeature('KPointer', '@koalaui/interop')
     }
     if (printer.language == Language.ARKTS) {
-        imports.addFeatures(["float64"], "@koalaui/common")
         imports.addFeatures(["NativeModule"], "#components")
         imports.addFeatures(["CallbackKind"], "CallbackKind")
         imports.addFeatures(['KStringPtr', 'nullptr', 'KInt', 'KPointer'], '@koalaui/interop')
