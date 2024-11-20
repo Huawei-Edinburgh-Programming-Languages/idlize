e07dff5bf712836de7947fa81fff9e87bc8a68e5
Korobeinikov Evgeny <korobeinikov.evgeny@huawei.com>
2024-11-20 17:47:29 +0300

    remove include i8

--- templates/bridge_generated_prologue.cc
+++ templates/bridge_generated_prologue.cc
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <_types/_uint8_t.h>
 #include <vector>
 #include <string>
 
