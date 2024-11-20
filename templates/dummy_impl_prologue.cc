4da602630c65de06cd437676bbe8bbf84ef9b0ce
Nikolay Igotti <nikolay.igotti@huawei.com>
2024-11-20 18:02:28 +0300

    Small update Signed-off-by: Nikolay Igotti <nikolay.igotti@huawei.com>

--- templates/dummy_impl_prologue.cc
+++ templates/dummy_impl_prologue.cc
@@ -770,17 +770,19 @@ Ark_Int32 IndexerChecker(Ark_VMContext vmContext, Ark_NodeHandle nodePtr) {
 void SetRangeUpdater(Ark_NodeHandle nodePtr, Ark_Int32 updaterId) {}
 void SetLazyItemIndexer(Ark_VMContext vmContext, Ark_NodeHandle nodePtr, Ark_Int32 indexerId) {}
 Ark_PipelineContext GetPipelineContext(Ark_NodeHandle node) {
-    return nullptr;
+    return (Ark_PipelineContext)42;
 }
 
 void SetVsyncCallback(Ark_PipelineContext pipelineContext, Ark_VsyncCallback callback) {
-    auto delayed_call = std::async(std::launch::async, [pipelineContext, callback] {
+    auto producer = std::thread([pipelineContext, callback] {
         while (true) {
             std::this_thread::sleep_for(1000ms);
             callback(pipelineContext);
         }
     });
+    producer.detach();
 }
+
 void SetChildTotalCount(Ark_NodeHandle node, Ark_Int32 totalCount) {}
 void ShowCrash(Ark_CharPtr message) {}
 }
