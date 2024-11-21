4c30dba5a1dedd90f729acb0e6d0c0183ac77fd2
twx1232375 <tsvetkova.elena@huawei-partners.com>
2024-11-21 12:47:52 +0300

    Edited synthetic tuple type naming

--- src/IDLVisitor.ts
+++ src/IDLVisitor.ts
@@ -562,7 +562,8 @@ export class IDLVisitor implements GenericVisitor<idl.IDLEntry[]> {
 
     serializeTupleType(node: ts.TupleTypeNode, nameSuggestion?: NameSuggestion, typeParameters?: ts.NodeArray<ts.Node>, withOperator: boolean = false): idl.IDLInterface {
         const properties = node.elements.map((it, index) => this.serializeTupleProperty(it, index, withOperator))
-        const syntheticName = `Tuple_${properties.map(it => this.computeTypeName(it.type)).join("_")}`
+        const file_basename = path.basename(this.sourceFile.fileName, ".d.ts")
+        const syntheticName = `Tuple_${properties.map(it => this.computeTypeName(it.type)).join("_")}_${file_basename}`
         const selectedName = selectName(nameSuggestion, syntheticName)
         return idl.createInterface(
             selectedName,
