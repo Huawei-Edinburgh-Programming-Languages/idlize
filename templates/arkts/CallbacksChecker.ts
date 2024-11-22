bd8a594052256d9dcda5b502d2c5260fcd0bfb08
Aleksandr Veselov <veselov.aleksandr@huawei.com>
2024-11-22 18:56:38 +0300

    WIP Fix  CallbacksChecker

--- templates/arkts/CallbacksChecker.ts
+++ templates/arkts/CallbacksChecker.ts
@@ -10,10 +10,10 @@ enum CallbackEventKind {
 
 const bufferSize = 1024
 const buffer = new Uint8Array(bufferSize)
-const deserializer = new Deserializer(buffer.buffer, bufferSize)
+const deserializer = new Deserializer(buffer.buffer as ArrayBuffer, bufferSize)
 export function checkArkoalaCallbacks() {
     while (true) {
-        let result = %NATIVE_MODULE_ACCESSOR%()._CheckArkoalaCallbackEvent(buffer, bufferSize)
+        let result = %NATIVE_MODULE_ACCESSOR%._CheckArkoalaCallbackEvent(buffer, bufferSize)
         if (result == 0) break
 
         deserializer.resetCurrentPosition()
