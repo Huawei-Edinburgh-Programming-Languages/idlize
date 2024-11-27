bcabd2ad0a74f5ffabef0ed0917f2b4b363588eb
Alexander Gorshenev <gorshenev.alexander@huawei.com>
2024-11-27 14:16:06 +0300

    Some tweaks in platdofrm.d.ts
    Signed-off-by: Alexander Gorshenev <gorshenev.alexander@huawei.com>

--- templates/arkts/platform.d.ts
+++ templates/arkts/platform.d.ts
@@ -1,50 +1,21 @@
 
-type buffer = byte[];
+// TODO: there should be no buffer in index-full.d.ts
+type buffer = ArrayBuffer
 
-declare const console: {
-    assert(value: any, message?: string, ...optionalParams: any[]): void;
-
-    clear(): void;
-
-    count(label?: string): void;
-
-    countReset(label?: string): void;
-
-    debug(message?: any, ...optionalParams: any[]): void;
-
-    // dir(obj: any, options?: InspectOptions): void;
-
-    dirxml(...data: any[]): void;
-
-    error(message?: any, ...optionalParams: any[]): void;
-
-    group(...label: any[]): void;
-
-    groupCollapsed(...label: any[]): void;
-
-    groupEnd(): void;
-
-    info(message?: any, ...optionalParams: any[]): void;
-
-    log(message?: any, ...optionalParams: any[]): void;
-
-    table(tabularData: any, properties?: ReadonlyArray<string>): void;
-
-    time(label?: string): void;
-
-    timeEnd(label?: string): void;
-
-    timeLog(label?: string, ...data: any[]): void;
-
-    trace(message?: any, ...optionalParams: any[]): void;
-
-    warn(message?: any, ...optionalParams: any[]): void;
-
-    profile(label?: string): void;
-
-    profileEnd(label?: string): void;
-
-    timeStamp(label?: string): void;
-
+// This is to workaround index-full.d.ts generation
+declare interface ContentModifier<T> {
+}
+declare interface LinearGradient {
+    angle?: number | KStringPtr;
+    direction?: GradientDirection;
+    colors: Array<[ ResourceColor, number ]>;
+    repeating?: boolean;
+}
+declare interface LayoutChild {
+    name: KStringPtr;
+    id: KStringPtr;
+    constraint: ConstraintSizeOptions;
+    borderInfo: LayoutBorderInfo;
+    position: Position;
 }
 
