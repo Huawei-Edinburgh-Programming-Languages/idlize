3181596593545d035df39eb77758ca05016b15ea
Alexander Gorshenev <gorshenev.alexander@huawei.com>
2024-11-27 18:32:34 +0300

    more
    Signed-off-by: Alexander Gorshenev <gorshenev.alexander@huawei.com>

--- templates/arkts/platform.d.ts
+++ templates/arkts/platform.d.ts
@@ -2,20 +2,3 @@
 // TODO: there should be no buffer in index-full.d.ts
 type buffer = ArrayBuffer
 
-// This is to workaround index-full.d.ts generation
-declare interface ContentModifier<T> {
-}
-declare interface LinearGradient {
-    angle?: number | KStringPtr;
-    direction?: GradientDirection;
-    colors: Array<[ ResourceColor, number ]>;
-    repeating?: boolean;
-}
-declare interface LayoutChild {
-    name: KStringPtr;
-    id: KStringPtr;
-    constraint: ConstraintSizeOptions;
-    borderInfo: LayoutBorderInfo;
-    position: Position;
-}
-
