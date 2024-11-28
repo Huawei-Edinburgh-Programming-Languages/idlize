3433dedd0e86e2581c7aba7c3dfb78d99225eb1a
Alexander Gorshenev <gorshenev.alexander@huawei.com>
2024-11-28 12:37:06 +0300

    more
    Signed-off-by: Alexander Gorshenev <gorshenev.alexander@huawei.com>

--- tests/subset/ets/common.d.ts
+++ tests/subset/ets/common.d.ts
@@ -458,15 +458,11 @@ declare class CommonMethod<T> {
 
     onGestureRecognizerJudgeBegin(callback: GestureRecognizerJudgeBeginCallback): T;
 
-    onClick(event: () => void): T;
+    // onClick(event: (event?: ClickEvent) => void): T;
+    //onClick(event: Callback<void> | Callback<ClickEvent>, distanceThreshold?: number): T;
+    // TODO: temporary!
+    onClick(event: ) => void): T;
 
-/*
-    // Uncomment for full sdk
-
-    onClick(event: (event?: ClickEvent) => void): T;
-
-    onClick(event?: Callback<ClickEvent>, distanceThreshold?: number): T;
-*/
     backgroundColor(value: ResourceColor): T;
 }
 
