975681d0aa765fa4154ee247f3537f0b459c13d7
Nikolay Igotti <igotti@gmail.com>
2024-12-04 09:30:48 +0300

    Simplify interop part of loader

--- peer_lib/sig/arkoala/framework/java/src/org/koalaui/arkoala/Application.java
+++ peer_lib/sig/arkoala/framework/java/src/org/koalaui/arkoala/Application.java
@@ -75,6 +75,10 @@ public class Application {
         }
     }
 
+    void emitEvent(int p0, int p1, int p2, int p3) {
+        System.out.println("JAVA: emitEvent");
+    }
+
     void updateState() {
         System.out.println("JAVA: updateState");
     }
