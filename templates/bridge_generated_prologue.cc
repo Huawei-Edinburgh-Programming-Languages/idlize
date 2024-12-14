9d0e54d24f5ba0a852cf778c6f03215e85761707
Aleksandr Veselov <veselov.aleksandr@huawei.com>
2024-12-14 23:02:03 +0300

    Add extra interop module name defines

--- templates/bridge_generated_prologue.cc
+++ templates/bridge_generated_prologue.cc
@@ -39,16 +39,3 @@ static const %CPP_PREFIX%ArkUIAccessors* GetAccessors() {
 namespace Generated {
     extern const GENERATED_ArkUIEventsAPI* GetArkUiEventsAPI();
 }
-
-void impl_EmulateTextInputEvent(KInt nodeId, const KStringPtr& text) {
-    /*
-    Ark_String str {
-        .chars = text.c_str(),
-        .length = static_cast<Ark_Int32>(text.length())
-    };
-    Opt_PreviewText preview;
-    preview.tag = ARK_TAG_UNDEFINED;
-    GetFullImpl()->getEventsAPI()->getTextInputEventsReceiver()->onChange(nodeId, str, preview);
-    */
-}
-KOALA_INTEROP_V2(EmulateTextInputEvent, KInt, KStringPtr)
