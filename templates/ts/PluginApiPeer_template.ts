a50f99eb714790bbe233374edc8cecf46b4591a6
chernishevvictor <chernishev.victor@h-partners.com>
2024-12-24 21:27:10 +0300

    separate generator(PluginApiGenerator) from OhosGenerator

---
+++ templates/ts/PluginApiPeer_template.ts
@@ -0,0 +1,5 @@
+import { nativeModule } from "../native/nativeModule"
+import { KInt, KPointer } from "@koalaui/interop"
+import { Finalizable, Serializer, PluginApiNativeModule } from "@?"
+
+%PEER_CONTENT%
