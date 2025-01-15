11c8dca3393e568c53f1a1f6b82a2c39ccc8a5e8
Peter Z <byte.fisher@huawei-partners.com>
2025-01-15 11:31:04 +0300

    Proper imports from @idlize/core

--- test/test-util.ts
+++ test/test-util.ts
@@ -1,18 +1,17 @@
 import * as path from "path"
-import { Language } from "@idlize/core"
+import { Language, toIDL } from "@idlize/core"
 import { createReferenceType, IDLEntry } from "@idlize/core/idl"
 import { PeerLibrary } from "../src/peer-generation/PeerLibrary"
 import { PeerFile } from "../src/peer-generation/PeerFile"
 
-import { readIDLFile } from "../core/test/test-util"
 
-export class IDLTestLibrary {
+export class IDLTestData {
     readonly peerLibrary: PeerLibrary
 
     constructor(idlFiles: string[]) {
         this.peerLibrary = new PeerLibrary(Language.TS)
         idlFiles.forEach(file =>
-            this.peerLibrary.files.push(new PeerFile(file, readIDLFile(path.join(__dirname, file)))))
+            this.peerLibrary.files.push(new PeerFile(file, toIDL(path.join(__dirname, file)))))
     }
 
     lookup<T extends IDLEntry>(name: string): T {
@@ -20,6 +19,6 @@ export class IDLTestLibrary {
     }
 }
 
-export function withFiles(idlFiles: string[], testFunc: (data: IDLTestLibrary) => void) {
-    testFunc(new IDLTestLibrary(idlFiles))
+export function withDataFrom(idlFiles: string | string[], testFunc: (data: IDLTestData) => void) {
+    testFunc(new IDLTestData(typeof idlFiles === "string" ? [idlFiles] : idlFiles))
 }
--- test/unions.ts
+++ test/unions.ts
@@ -1,10 +1,10 @@
 import { Language } from "@idlize/core"
 import { IDLInterface } from '@idlize/core/idl'
 import { createLanguageWriter } from '../src/peer-generation/LanguageWriters'
-import { withFiles } from "./test-util"
+import { withDataFrom } from "./test-util"
 
 test("Test union discrimination", () => {
-    withFiles(["unions.idl"], data => {
+    withDataFrom("unions.idl", data => {
         const writer = createLanguageWriter(Language.TS, data.peerLibrary)
         const testCases: IDLInterface = data.lookup("TestCases")
         testCases.properties.forEach(f =>
