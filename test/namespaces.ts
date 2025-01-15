6eb0030713adb984a49f3073b39b533abdc3db5c
Peter Z <byte.fisher@huawei-partners.com>
2025-01-15 15:49:10 +0300

    Better grouping of tests

--- test/namespaces.ts
+++ test/namespaces.ts
@@ -2,25 +2,27 @@ import { createReferenceType, isEnum, isEnumMember } from "@idlize/core"
 import { withDataFrom } from "./test-util"
 import { assert } from "chai"
 
-withDataFrom("namespaces.idl", data => {
-    test("Toplevel declaration lookup", () => {
-        const ref = createReferenceType("Size")
-        const decl = data.peerLibrary.resolveTypeReference(ref)
-        assert(decl && isEnum(decl))
-        assert(decl.elements.every(e => !e.name.includes("BUBBLE")))
-    })
+suite("Reference resolution", () => {
+    withDataFrom("namespaces.idl", data => {
+        test("Toplevel declaration lookup", () => {
+            const ref = createReferenceType("Size")
+            const decl = data.peerLibrary.resolveTypeReference(ref)
+            assert(decl && isEnum(decl))
+            assert(decl.elements.every(e => !e.name.includes("BUBBLE")))
+        })
 
-    test("Namespace lookup", () => {
-        const nsRef = createReferenceType("Bubble.Size")
-        const decl = data.peerLibrary.resolveTypeReference(nsRef)
-        assert(decl && isEnum(decl))
-        assert(decl.elements.every(e => e.name.endsWith("_BUBBLE")))
-    })
+        test("Namespace lookup", () => {
+            const nsRef = createReferenceType("Bubble.Size")
+            const decl = data.peerLibrary.resolveTypeReference(nsRef)
+            assert(decl && isEnum(decl))
+            assert(decl.elements.every(e => e.name.endsWith("_BUBBLE")))
+        })
 
-    test("Enum member lookup", () => {
-        const enumRef = createReferenceType("Size.LIL")
-        const decl = data.peerLibrary.resolveTypeReference(enumRef)
-        assert(decl && isEnumMember(decl))
-        assert.equal(decl.name, "LIL")
+        test("Enum member lookup", () => {
+            const enumRef = createReferenceType("Size.LIL")
+            const decl = data.peerLibrary.resolveTypeReference(enumRef)
+            assert(decl && isEnumMember(decl))
+            assert.equal(decl.name, "LIL")
+        })
     })
 })
--- test/unions.ts
+++ test/unions.ts
@@ -3,12 +3,13 @@ import { IDLInterface } from '@idlize/core/idl'
 import { createLanguageWriter } from '../src/peer-generation/LanguageWriters'
 import { withDataFrom } from "./test-util"
 
-test("Test union discrimination", () => {
+suite("Unions", () => {
     withDataFrom("unions.idl", data => {
-        const writer = createLanguageWriter(Language.TS, data.peerLibrary)
-        const testCases: IDLInterface = data.lookup("TestCases")
-        testCases.properties.forEach(f =>
-            data.peerLibrary.typeConvertor("", f.type).convertorSerialize("", "", writer))
+        test("Union discrimination", () => {
+            const writer = createLanguageWriter(Language.TS, data.peerLibrary)
+            const testCases: IDLInterface = data.lookup("TestCases")
+            testCases.properties.forEach(f =>
+                data.peerLibrary.typeConvertor("", f.type).convertorSerialize("", "", writer))
+        })
     })
 })
-
