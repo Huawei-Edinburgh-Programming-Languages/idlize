c59600decb1d13f03c7419b55cbb6d5decc9d0d6
Nikolay Igotti <nikolay.igotti@huawei.com>
2025-01-20 16:57:30 +0300

    Release version 2.0.1

--- tools/release.mjs
+++ tools/release.mjs
@@ -88,7 +88,7 @@ function run() {
         throw new Error("Failed to publish idlize package")
     }
 
-    console.log(`> Link: https://nexus.bz-openlab.ru:10443/repository/koala-npm/%40azanat/idlize/-/idlize-${next.toString()}.tgz`)
+    console.log(`> Link: https://nexus.bz-openlab.ru:10443/repository/koala-npm/%40idlize/arkgen/-/idlize-${next.toString()}.tgz`)
     console.log("$ git push")
 
 }
