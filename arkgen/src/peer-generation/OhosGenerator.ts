032fde8bc76863e8faa7538ea25d510661609650
twx1232375 <tsvetkova.elena@huawei-partners.com>
2025-01-23 16:00:45 +0300

    generate all structs for ohos

--- arkgen/src/peer-generation/OhosGenerator.ts
+++ arkgen/src/peer-generation/OhosGenerator.ts
@@ -651,7 +651,7 @@ class OHOSVisitor {
         )
 
         let toStringsPrinter = createLanguageWriter(Language.CPP, this.library)
-        new StructPrinter(this.library).generateStructs(this.hWriter, this.hWriter.printer, toStringsPrinter)
+        new StructPrinter(this.library).generateStructs(this.hWriter, this.hWriter.printer, toStringsPrinter, true)
         this.cppWriter.concat(toStringsPrinter)
         const prefix = generatorConfiguration().param("TypePrefix") + this.library.libraryPrefix
         writeSerializer(this.library, this.cppWriter, prefix)
