b568ff952bdc85b1863055245f2d59112a3d4740
Aleksandr Veselov <veselov.aleksandr@huawei.com>
2025-01-24 19:19:39 +0300

    WIP fix path to native modules classes

--- arkgen/demos/xml/generated/arkts/xmlNative.ts
+++ arkgen/demos/xml/generated/arkts/xmlNative.ts
@@ -10,7 +10,7 @@ export enum CallbackKind {
 export class XMLNativeModule {
     static {
         loadLibrary("XML_NativeBridgeArk")
-        XMLNativeModule.init(["xmlNative/XMLNativeModule", "xmlNative/ArkUINativeModule"])
+        XMLNativeModule.init(["@koalaui/interop/generated/arkts/xmlNative/XMLNativeModule", "@koalaui/interop/generated/arkts/xmlNative/ArkUINativeModule"])
     }
 
     static native init(modules: string[]): void
