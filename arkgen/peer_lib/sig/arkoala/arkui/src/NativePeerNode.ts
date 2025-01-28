ead7a0334f800e720c56430dca86cfc3ecc3e7bf
Nikolay Igotti <nikolay.igotti@huawei.com>
2025-01-28 13:49:26 +0300

    Build Signed-off-by: Nikolay Igotti <nikolay.igotti@huawei.com>

--- arkgen/peer_lib/sig/arkoala/arkui/src/NativePeerNode.ts
+++ arkgen/peer_lib/sig/arkoala/arkui/src/NativePeerNode.ts
@@ -1,7 +1,6 @@
 import { int32 } from "@koalaui/common"
 import { pointer, Finalizable } from "@koalaui/interop"
 import { PeerNode } from "./PeerNode"
-import { ArkUINodeType } from "./peers/ArkUINodeType"
 import { ArkUINativeModule } from "@koalaui/arkoala";
 
 export class NativePeerNode extends Finalizable {
