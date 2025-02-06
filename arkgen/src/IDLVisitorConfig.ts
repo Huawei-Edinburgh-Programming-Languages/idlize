81ce54e79403e64826c5443ce2826d66a9095135
kirillberezin <berezin.kirill@huawei-partners.com>
2025-02-06 09:50:18 +0300

    add signature for scrollerCommon.onDidScroll
    Signed-off-by: kirillberezin <berezin.kirill@huawei-partners.com>

--- arkgen/src/IDLVisitorConfig.ts
+++ arkgen/src/IDLVisitorConfig.ts
@@ -48,11 +48,11 @@ const parameterTypeReplacements: ((clazz: string, method: string, parameter: str
     },
     (clazz, method, parameter) => {
         if (clazz === "ScrollableCommonMethod" && method === "onDidScroll" && parameter === "handler")
-            return [idl.createOptionalType(idl.createReferenceType("ScrollOnScrollCallback"))]
+            return [idl.createOptionalType(idl.createReferenceType("OnScrollCallback"))]
     },
     (clazz, method, parameter) => {
         if (clazz === "ScrollableCommonMethod" && method === "onScroll" && parameter === "event")
-            return [idl.createReferenceType("Callback_Number_Number_Void")]
+            return [idl.createReferenceType("Callback_Number_ScrollState_Void")]
     },
 
     (clazz, method, parameter) => {
@@ -62,6 +62,10 @@ const parameterTypeReplacements: ((clazz: string, method: string, parameter: str
     (clazz, method, parameter) => {
         if (clazz === "ScrollAttribute" && method === "onDidScroll" && parameter === "handler")
             return [idl.createOptionalType(idl.createReferenceType("ScrollOnScrollCallback"))]
+    },
+    (clazz, method, parameter) => {
+        if (clazz === "ScrollAttribute" && method === "onScroll" && parameter === "event")
+            return [idl.createReferenceType("Callback_Number_Number_Void")]
     },
 
     (clazz, method, parameter) => {
