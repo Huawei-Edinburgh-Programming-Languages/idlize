e75873cb07bb7442fe92aae4e927805b9381219e
scherbatiyalexander <scherbatiy.alexander@huawei-partners.com>
2025-02-06 20:56:03 +0300

    Remove comment

--- arkgen/src/peer-generation/printers/SerializerPrinter.ts
+++ arkgen/src/peer-generation/printers/SerializerPrinter.ts
@@ -87,7 +87,6 @@ class SerializerPrinter {
             const type = flattenUnionType(this.library, it.type)
             // optional is forced as a workaround for non initialized class properties in ArkTS (see PR 1465)
             const forceOptional = writer.language === Language.ARKTS && idl.isClassSubkind(target)
-            writer.print(`// serialize ${target.name}, is class: ${idl.isClassSubkind(target)}, property: ${it.name}, optional: ${it.isOptional}, force optional: ${forceOptional}`)
             let typeConvertor = this.library.typeConvertor(`value`, type, it.isOptional || forceOptional)
 
             let memberAccess = writer.makeString(`value.${writer.escapeKeyword(it.name)}`)
