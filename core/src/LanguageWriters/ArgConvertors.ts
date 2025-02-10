a5db21b1bbe059b1813e725b1566cd45e511e7fc
Aleksandr Veselov <veselov.aleksandr@huawei.com>
2025-02-10 12:18:05 +0300

    Generate interop for bigint

--- core/src/LanguageWriters/ArgConvertors.ts
+++ core/src/LanguageWriters/ArgConvertors.ts
@@ -301,6 +301,37 @@ export class NumericConvertor extends BaseArgConvertor {
     }
 }
 
+export class BigIntToU64Convertor extends BaseArgConvertor {
+    constructor(param: string) {
+        // TODO: as we pass tagged values - request serialization to array for now.
+        // Optimize me later!
+        super(idl.IDLBigintType, [RuntimeType.BIGINT], false, false, param)
+    }
+    convertorArg(param: string, writer: LanguageWriter): string {
+        return writer.escapeKeyword(param)
+    }
+    convertorSerialize(param: string, value: string, printer: LanguageWriter): void {
+        throw "unimplemented"
+        printer.writeMethodCall(`${param}Serializer`, "writeNumber", [value])
+    }
+    convertorDeserialize(bufferName: string, deserializerName: string, assigneer: ExpressionAssigner, writer: LanguageWriter): LanguageStatement {
+        throw "unimplemented"
+        return assigneer(writer.makeCast(
+            writer.makeString(`${deserializerName}.readNumber()`),
+            this.idlType, { optional: false })
+        )
+    }
+    nativeType(): idl.IDLType {
+        return idl.IDLU64Type
+    }
+    interopType(): idl.IDLType {
+        return idl.IDLU64Type
+    }
+    isPointerType(): boolean {
+        return false
+    }
+}
+
 export class PointerConvertor extends BaseArgConvertor {
     constructor(param: string) {
         // check numericPrimitiveTypes.include(type)
