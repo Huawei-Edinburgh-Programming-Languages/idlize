bfbf6727145237e4d571e484312a038c3205d7e2
kolegovilya <kolegov.ilya@huawei.com>
2025-02-24 19:08:23 +0300

    version 0

--- ohosgen/demos/mediaquery/src/cpp/mediaqueryImpl.cc
+++ ohosgen/demos/mediaquery/src/cpp/mediaqueryImpl.cc
@@ -26,6 +26,6 @@ OH_Boolean MediaQueryListener_getMatchesImpl(OH_NativePointer thisPtr) {
 OH_String MediaQueryListener_getMediaImpl(OH_NativePointer thisPtr) {
     return {};
 }
-OH_NativePointer GlobalScope_mediaquery_ohos_mediaquery_matchMediaSyncImpl(const OH_String* condition) {
+OH_NativePointer GlobalScope_mediaquery_matchMediaSyncImpl(const OH_String* condition) {
     return {};
 }
