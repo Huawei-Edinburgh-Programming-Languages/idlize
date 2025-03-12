da183130082d15317cc692aec9ff9a900a3d95ca
tsvetkovaelena <tsvetkova.elena@huawei-partners.com>
2025-03-12 11:21:40 +0800

    added logs

--- demo_helen/src/demo.ts
+++ demo_helen/src/demo.ts
@@ -27,3 +27,24 @@ function operationWithArrayOfInterface_(value: Array<test_namespace.UtilityInter
 function operationWithArrayOfInterface__() {
     return new Array<test_namespace.UtilityInterface>()
 }
+
+/**
+ * > arktsc --arktsconfig arktsconfig.json
+ * 
+    [TID 0aad0c] E/es2panda:   In /home/twx1232375/idlize/demo_helen/src/demo.ts:
+    [TID 0aad0c] E/es2panda:     IdentifierHasVariable:
+    [TID 0aad0c] E/es2panda:       NULL_VARIABLE(AstNodeType::IDENTIFIER, line 16)
+    [TID 0aad0c] F/es2panda: ASTVerifier found broken invariants. You may want to pass '--ast-verifier:json' option for more verbose output.
+    FATAL ERROR
+    Backtrace [tid=699660]:
+    Received signal: SIGABRT from '$ /home/twx1232375/idlize/demo_helen> /home/twx1232375/idlize/external/incremental/tools/panda/node_modules/@panda/sdk/linux_host_tools/bin/es2panda --stdlib /home/twx1232375/idlize/external/incremental/tools/panda/node_modules/@panda/sdk/ets/stdlib --extension sts --list-files --arktsconfig arktsconfig.json'
+
+
+ * > arktsc --version
+
+    Es2panda Version 0.1
+
+    Build date: 2025-03-08_05:26:35
+    Last commit hash: 25f435a2b6fd71fda90bece4921cd05cc4111832
+
+ */
