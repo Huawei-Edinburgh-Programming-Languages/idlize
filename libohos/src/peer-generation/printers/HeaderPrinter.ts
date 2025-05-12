68eb7e3ead833d3881c1be0908c7565654b16f9c
kolegovilya <kolegov.ilya@huawei.com>
2025-05-12 13:55:45 +0300

    native generation

--- libohos/src/peer-generation/printers/HeaderPrinter.ts
+++ libohos/src/peer-generation/printers/HeaderPrinter.ts
@@ -16,12 +16,14 @@
 import { IndentedPrinter, camelCaseToUpperSnakeCase, maybeOptional, Language, CppConvertor,
     createConstructPeerMethod, createDestroyPeerMethod, PeerClass, PeerMethod, PeerLibrary, CppReturnTypeConvertor,
     MaterializedClass,
+    getQualifiedName,
 } from '@idlizer/core'
 import { getNodeTypes } from "../FileGenerators";
 import { peerGeneratorConfiguration} from "../../DefaultConfiguration";
 import { printMethodDeclaration } from "../LanguageWriters";
 import { createGlobalScopeLegacy } from '../GlobalScopeUtils';
 import { collectPeersForFile } from '../PeersCollector';
+import { getAccessorName, getDeclarationUniqueName } from './NativeUtils';
 
 export function generateEventReceiverName(componentName: string) {
     return `${peerGeneratorConfiguration().cppPrefix}ArkUI${componentName}EventsReceiver`
@@ -65,7 +67,9 @@ export class HeaderVisitor {
         this.accessorsList.pushIndent()
         this.library.materializedClasses.forEach(c => {
             this.printAccessor(c)
-            this.accessorsList.print(`const ${peerGeneratorConfiguration().cppPrefix}ArkUI${c.className}Accessor* (*get${c.className}Accessor)();`)
+            const accessorName = getAccessorName(c.decl)
+            const className = getDeclarationUniqueName(c.decl)
+            this.accessorsList.print(`const ${accessorName}* (*get${className}Accessor)();`)
         })
         const globals = createGlobalScopeLegacy(this.library)
         if (globals.methods.length) {
@@ -76,8 +80,7 @@ export class HeaderVisitor {
     }
 
     private printAccessor(clazz: MaterializedClass) {
-        let peerName = `${clazz.className}Peer`
-        let accessorName = `${peerGeneratorConfiguration().cppPrefix}ArkUI${clazz.className}Accessor`
+        const accessorName = getAccessorName(clazz.decl)
         this.api.print(`typedef struct ${accessorName} {`)
         this.api.pushIndent()
         const mDestroyPeer = createDestroyPeerMethod(clazz)
