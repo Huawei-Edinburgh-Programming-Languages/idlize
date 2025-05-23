778bb7761afe011b899f34da8a05239525776b86
kolegovilya <kolegov.ilya@huawei.com>
2025-05-23 12:47:41 +0300

    remove top type

---
+++ core/src/peer-generation/getSuperType.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as idl from '../idl'
+import { ReferenceResolver } from './ReferenceResolver'
+
+function getSuperCandidates(declaration:idl.IDLInterface, resolver: ReferenceResolver): idl.IDLInterface[] {
+    return declaration.inheritance
+        .map(it => resolver.resolveTypeReference(it))
+        .filter(it => it && idl.isInterface(it) && idl.isClassSubkind(it))
+        .map(it => it as idl.IDLInterface)
+}
+
+export function getSuper(declaration:idl.IDLInterface, resolver: ReferenceResolver): idl.IDLInterface | undefined {
+    if (idl.isClassSubkind(declaration)) {
+        const found = declaration.inheritance.find(it => idl.hasExtAttribute(it, idl.IDLExtendedAttributes.Extends))
+        if (found) {
+            const resolved = resolver.resolveTypeReference(found)
+            if (resolved && idl.isInterface(resolved)) {
+                return resolved
+            }
+        }
+        const candidates = getSuperCandidates(declaration, resolver)
+        if (candidates.length > 0) {
+            return candidates[0]
+        }
+        return undefined
+    }
+    const fst = declaration.inheritance[0]
+    if (!fst) {
+        return undefined
+    }
+    const resolved = resolver.resolveTypeReference(fst)
+    if (!resolved || !idl.isInterface(resolved)) {
+        return undefined
+    }
+    return resolved
+}
--- libohos/src/peer-generation/idl/IdlPeerGeneratorVisitor.ts
+++ libohos/src/peer-generation/idl/IdlPeerGeneratorVisitor.ts
@@ -24,7 +24,8 @@ import {
     qualifiedName,
     isStaticMaterialized,
     isInCurrentModule,
-    ArgumentModifier
+    ArgumentModifier,
+    getSuper
 } from '@idlizer/core'
 import { ArgConvertor, PeerLibrary } from "@idlizer/core"
 import { createOutArgConvertor } from "../PromiseConvertors"
@@ -138,10 +139,7 @@ export class IdlPeerProcessor {
     private getBuilderMethods(target: idl.IDLInterface, className?: string): Method[] {
         return [
             ...target.inheritance
-                .filter(it => it !== idl.IDLTopType)
                 .filter(it => {
-                    if (it.name === idl.IDLTopType.name)
-                        return false
                     if (!this.library.resolveTypeReference(it))
                         console.log(`Cannot resolve ${it.name}`)
                     return true
@@ -177,11 +175,12 @@ export class IdlPeerProcessor {
 
         const isDeclInterface = idl.isInterfaceSubkind(decl) && !isStaticMaterialized
         const implemenationParentName = isDeclInterface ? getInternalClassName(decl.name) : decl.name
-        let superType = idl.getSuperType(decl)
+        let resolvedType = getSuper(decl, this.library)
         const interfaces: idl.IDLReferenceType[] = []
         const propertiesFromInterface: idl.IDLProperty[] = []
-        if (superType) {
-            const resolvedType = this.library.resolveTypeReference(superType)
+        let superType = undefined
+        if (resolvedType) {
+            superType = idl.createReferenceType(resolvedType)
             if (!resolvedType || !idl.isInterface(resolvedType) || !isMaterialized(resolvedType, this.library)) {
                 propertiesFromInterface.push(...getUniquePropertiesFromSuperTypes(decl, this.library))
                 interfaces.push(superType)
@@ -344,9 +343,8 @@ export function isCommonMethodOrSubclass(library: PeerLibrary, decl?: idl.IDLEnt
     if (!decl || !idl.isInterface(decl))
         return false
     let isSubclass = isRoot(decl.name)
-    const superType = idl.getSuperType(decl)
-    if (superType && idl.isReferenceType(superType)) {
-        const superDecl = library.resolveTypeReference(superType)
+    const superDecl = getSuper(decl, library)
+    if (superDecl) {
         isSubclass ||= isCommonMethodOrSubclass(library, superDecl)
     }
     return isSubclass
@@ -376,12 +374,11 @@ function generateSignature(
 }
 
 export function forEachSuperType(declaration: idl.IDLInterface, resolver: ReferenceResolver, callback: (superType: idl.IDLInterface) => void) {
-    if (!idl.hasSuperType(declaration)) return
-    const resolvedType = resolver.resolveTypeReference(idl.getSuperType(declaration)!) as (idl.IDLInterface | undefined)
-    if (!resolvedType) return
+    const superDecl = getSuper(declaration, resolver)
+    if (!superDecl) return
 
-    callback(resolvedType as idl.IDLInterface)
-    forEachSuperType(resolvedType as idl.IDLInterface, resolver, callback)
+    callback(superDecl)
+    forEachSuperType(superDecl, resolver, callback)
 }
 
 export function getUniquePropertiesFromSuperTypes(declaration: idl.IDLInterface, resolver: ReferenceResolver): idl.IDLProperty[] {
