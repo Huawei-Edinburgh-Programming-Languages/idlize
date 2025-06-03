a3455415daaec5482f03f956425ecac4fbadf54e
kolegovilya <kolegov.ilya@huawei.com>
2025-06-03 17:18:55 +0300

    small typo fix

--- libohos/templates/any_api.h
+++ libohos/templates/any_api.h
@@ -1,7 +1,7 @@
 #ifndef GENERATED_FOUNDATION_ACE_FRAMEWORKS_CORE_INTERFACES_ANY_API_H
 #define GENERATED_FOUNDATION_ACE_FRAMEWORKS_CORE_INTERFACES_ANY_API_H
 #include <stdint.h>
-// todo remove after migration to OH_AnyAPI to be consistant between arkoala and ohos apis
+// todo remove after migration to OH_AnyAPI to be consistent between arkoala and ohos apis
 struct Ark_AnyAPI {
     int32_t version;
 };
