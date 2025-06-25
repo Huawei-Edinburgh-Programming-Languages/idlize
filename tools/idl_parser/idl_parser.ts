2b38a2d79d5d6e108ee4825983915dbb82033cf2
vladimirrybakov <vladimir.rybakov@bell-sw.com>
2025-06-25 13:53:42 +0300

    IDL parser initial commit

---
+++ tools/idl_parser/idl_parser.ts
@@ -0,0 +1,16 @@
+
+import * as lex from "./idl_lexer";
+
+function test() {
+  const s: string = "int i = 0; while (i < 10) print(i++);";
+  lex.init(s);
+  console.log(s);
+
+  let t: lex.Token;
+  do {
+    t = lex.getToken();
+    console.log(t);    
+  } while (t != lex.Token.tError && t != lex.Token.tEnd);
+}
+
+test();
