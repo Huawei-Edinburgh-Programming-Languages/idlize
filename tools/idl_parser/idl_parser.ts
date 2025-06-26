56c175e19e91f3bc1d95a72ef5e970f779d62572
vladimirrybakov <vladimir.rybakov@bell-sw.com>
2025-06-26 20:14:57 +0300

    Parser in work

--- tools/idl_parser/idl_parser.ts
+++ tools/idl_parser/idl_parser.ts
@@ -1,17 +1,47 @@
 
 import * as lex from "./idl_lexer";
 
+function Definitions() {
+  while (1) {
+    ExtendedAttributeList();
+    Definition();
+  } //Definitions
+  //or empty
+}
+
+function Definition() {
+  CallbackOrInterfaceOrMixin();
+  Namespace();
+  Partial();
+  Dictionary();
+  Enum();
+  Typedef();
+  IncludesStatement();
+}
+
+function CallbackOrInterfaceOrMixin() {}
+
+function Namespace() {}
+function Partial() {}
+function Dictionary() {}
+function Enum() {}
+function Typedef() {}
+function IncludesStatement() {}
+
 function test() {
   const s: string = "int i = 0; while (i < 10) print(i++);";
 
   lex.init(s);
   console.log(s);
 
+  Definitions();
+/*
   let t: lex.Token;
   do {
     t = lex.getToken();
     console.log(t);    
   } while (t != lex.Token.tError && t != lex.Token.tEnd);
+*/
 }
 
 test();
