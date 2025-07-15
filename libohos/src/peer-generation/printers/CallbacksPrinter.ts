605f25f6d49e2b6697c77d07ea42f3fbd92d00d5
pavelmironchik <pavel.mironchik@huawei.com>
2025-07-15 16:22:29 +0000

    !2391 fix forgotten deserializeAndCallSync case conditions

--- libohos/src/peer-generation/printers/CallbacksPrinter.ts
+++ libohos/src/peer-generation/printers/CallbacksPrinter.ts
@@ -407,7 +407,7 @@ class DeserializeCallbacksVisitor {
                             ? [`vmContext`, `thisArray`, `thisLength`]
                             : [`thisDeserializer`]
                         const callbackKindValue = generateCallbackKindAccess(callback, this.writer.language)
-                        writer.print(`case ${generateCallbackKindValue(callback)}/*${callbackKindValue}*/: return deserializeAndCallSync${callback.name}(${args.join(', ')});`)
+                        writer.print(`case ${callbackKindValue}: return deserializeAndCallSync${callback.name}(${args.join(', ')});`)
                     }
                     writer.popIndent()
                     writer.print(`}`)
